331487b3f6101190989a55736d536d270f83885d
ZAndKnight <263382008@qq.com>
2026-01-18 23:40:26 +0900

    删除与浏览

---
+++ Assets/Thrid2dCapture/Editor/Tools/DeleteTmpPNG.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace com.knight.thrid2dcapture
+{
+    public class DeleteTmpPNG : EditorWindow
+    {
+        public TextAsset JsonAsset;
+
+        public void OnGUI()
+        {
+            JsonAsset = (TextAsset)EditorGUILayout.ObjectField("Json Asset", JsonAsset, typeof(TextAsset), false);
+            if (GUILayout.Button("Delete Tmp PNG"))
+            {
+                if (JsonAsset == null)
+                {
+                    Debug.LogError("Please assign a Json Asset.");
+                    return;
+                }
+
+                var jsonText = JsonAsset.text;
+                var genJson = JsonConvert.DeserializeObject<GenJson>(jsonText);
+                if (genJson == null)
+                {
+                    Debug.LogError("Invaild Json File");
+                    return;
+                }
+
+                DeleteTmp(genJson);
+            }
+        }
+
+        private void DeleteTmp(GenJson json)
+        {
+            var deleteFileResults = new List<string>();
+            foreach (var actJson in json.ActionJsons)
+            {
+                foreach (var path in actJson.AnimationClipPaths)
+                {
+                    var dire = Path.GetDirectoryName(path);
+                    var guids = AssetDatabase.FindAssets("t:Texture2D", new string[] { dire });
+                    var deleteList = new string[guids.Length];
+                    for (var i = 0; i < deleteList.Length; ++i)
+                    {
+                        var guid = guids[i];
+                        var deleteFileName = AssetDatabase.GUIDToAssetPath(guid);
+                        deleteList[i] = deleteFileName;
+                    }
+                    var deleteResults = new List<string>();
+                    var result = AssetDatabase.DeleteAssets(deleteList, deleteResults);
+                    if (result)
+                    {
+                        deleteFileResults.AddRange(deleteResults);
+                    }
+                }
+            }
+
+            if (deleteFileResults.Count == 0)
+            {
+                Debug.Log($"Delete finished: {json.BasePath}");
+            }
+            else
+            {
+                foreach (var deleteFileResult in deleteFileResults)
+                {
+                    Debug.LogError($"FILE DELETE FAILED: {deleteFileResult}");
+                }
+            }
+        }
+
+        [MenuItem("Thrid2dCapture/Tools/Delete Tmp PNG")]
+        public static void ShowWindow()
+        {
+            GetWindow<DeleteTmpPNG>("Delete Tmp PNG");
+        }
+    }
+}
---
+++ Assets/Thrid2dCapture/Editor/Tools/EasyTools.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace com.knight.thrid2dcapture
+{
+    public static class EasyTools
+    {
+        public const string RENDER_KEY = "_MASKSWITCH";
+
+        [MenuItem("Thrid2dCapture/Tools/Switch Render Key")]
+        public static void SwitchRenderKey()
+        {
+            var key = Shader.GetGlobalFloat(RENDER_KEY);
+            Shader.SetGlobalFloat(RENDER_KEY, 1 - key);
+            Debug.Log($"Switch Render Key ({RENDER_KEY}) to: {1 - key}");
+        }
+    }
+}
